89009c56199abe4ae5475555c29507f42ef43faf
Phangg <109324421+Phangg@users.noreply.github.com>
2022-09-11 20:33:11 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 224 ms, Memory: 33692 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+N = int(sys.stdin.readline())
+hq = []
+for _ in range(N):
+    heapq.heappush(hq, int(sys.stdin.readline()))
+
+ans = 0
+while len(hq) > 1:
+    a = heapq.heappop(hq)           # 최소 값 a 로 지정 (hq 에서 삭제 됨)
+    b = heapq.heappop(hq)           # a가 사라진 뒤, 최소 값 b 로 지정 (hq 에서 삭제 됨)
+    ans += (a + b)                  # a + b 값 저장
+    heapq.heappush(hq, a+b)         # a + b 를 hq 에 삽입 (이 값을 포함해서 위 과정 반복)
+print(ans)
