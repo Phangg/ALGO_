8d97befd87592761199b35035aff21728d0658ab
Phangg <109324421+Phangg@users.noreply.github.com>
2022-09-12 16:26:33 +0900

    [Silver IV] Title: 수 찾기, Time: 208 ms, Memory: 50092 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(sys.stdin.readline())
+N_lst = set(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+M_lst = list(map(int, sys.stdin.readline().split()))
+
+for m in M_lst:
+    if m in N_lst:
+        print(1)
+    else:
+        print(0)
