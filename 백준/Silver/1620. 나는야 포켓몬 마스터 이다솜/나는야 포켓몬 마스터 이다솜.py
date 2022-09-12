b551f8ff984d598947f6d92a9f14c6b726e47fd1
Phangg <109324421+Phangg@users.noreply.github.com>
2022-09-12 23:26:10 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 276 ms, Memory: 54844 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,16 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+n_dict = dict()
+n_lst = []
+for i in range(N):
+    x = str(sys.stdin.readline().rstrip())
+    n_dict[x] = i
+    n_lst.append(x)
+m_lst = [sys.stdin.readline().rstrip() for _ in range(M)]
+
+for m in m_lst:
+    if m.isnumeric():
+        print(n_lst[int(m)-1])
+    else:
+        print(n_dict[m]+1)
