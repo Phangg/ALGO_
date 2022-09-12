38e20fd2fb3fa4a9659aa2e8f81590ad42818a40
Phangg <109324421+Phangg@users.noreply.github.com>
2022-09-12 14:56:29 +0900

    [Bronze II] Title: 음계, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,15 @@
+import sys
+lst = list(map(int, sys.stdin.readline().split()))
+
+a, d = 0, 0
+for i in range(1, 8):
+    if lst[i] > lst[i-1]:
+        a += 1
+    elif lst[i] < lst[i-1]:
+        d += 1
+if a == 7:
+    print('ascending')
+elif d == 7:
+    print('descending')
+else:
+    print('mixed')
