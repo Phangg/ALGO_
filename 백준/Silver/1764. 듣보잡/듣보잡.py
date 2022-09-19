a18a857aaa4865c824b2a231debb2ae3a42f179a
Phangg <109324421+Phangg@users.noreply.github.com>
2022-09-19 11:14:35 +0900

    [Silver IV] Title: 듣보잡, Time: 128 ms, Memory: 44160 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,13 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+no_h = set(sys.stdin.readline().rstrip() for _ in range(N))
+no_s = set(sys.stdin.readline().rstrip() for _ in range(M))
+
+x = no_h - no_s
+
+ans = no_h - x
+ans = sorted(ans)
+print(len(ans))
+for a in ans:
+    print(a)
