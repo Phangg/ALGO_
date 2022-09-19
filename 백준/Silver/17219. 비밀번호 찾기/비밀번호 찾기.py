6a9e81f93f97dba237181f3b6454c566ad0c266e
Phangg <109324421+Phangg@users.noreply.github.com>
2022-09-19 16:33:34 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 264 ms, Memory: 49788 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,9 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+dic = dict()
+for _ in range(N):
+    domain, pw = sys.stdin.readline().split()
+    dic[domain] = pw
+for _ in range(M):
+    print(dic[sys.stdin.readline().rstrip()])
