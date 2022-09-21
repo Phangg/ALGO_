c19aff29eda60619cb4486e07ce1558aa6a060cc
Phangg <109324421+Phangg@users.noreply.github.com>
2022-09-21 15:15:50 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 316 ms, Memory: 40596 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,15 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+nums = list(map(int, sys.stdin.readline().split()))
+hap = [0] * N
+plus = 0
+for idx, num in enumerate(nums):
+    plus += num
+    hap[idx] = plus
+for _ in range(M):
+    s, e = map(int, sys.stdin.readline().split())
+    if s-1:
+        print(hap[e-1] - hap[s-1-1])
+    else:
+        print(hap[e-1])
