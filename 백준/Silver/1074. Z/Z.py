379cdede994b70597123fb72cfe13b928d0bd191
Phangg <109324421+Phangg@users.noreply.github.com>
2022-09-28 22:49:55 +0900

    [Silver I] Title: Z, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,6 +1,7 @@
 import sys
 
-def divide(n, r, c, cnt):
+def divide(n, r, c):
+    global cnt
     if n == 2:
         if r == 0 and c == 0:
             cnt += 0
@@ -10,19 +11,22 @@ def divide(n, r, c, cnt):
             cnt += 2
         else:
             cnt += 3
-        return cnt
     else:
-        if r < n//2 and c < n//2:                                           # 좌 상
-            cnt = divide(n//2, r, c, cnt)
-        elif r < n//2 and c >= n//2:                                        # 우 상
-            cnt = divide(n//2, r, c-(n//2), cnt+(n//2)**2)
-        elif r >= n//2 and c < n//2:                                        # 좌 하
-            cnt = divide(n//2, r-(n//2), c, cnt+((n//2)**2)*2)
-        elif r >= n//2 and c >= n//2:                                       # 우 하
-            cnt = divide(n//2, r-(n//2), c-(n//2), cnt+(((n//2)**2)*3))     # ex) N=3 , n=8, 각 사각형의 첫값 시작 = 16*i
-        return cnt
+        if r < n//2 and c < n//2:                           # 좌 상
+            divide(n//2, r, c)
+        elif r < n//2 and c >= n//2:                        # 우 상
+            cnt += (n*n)//4
+            divide(n//2, r, c-(n//2))
+        elif r >= n//2 and c < n//2:                        # 좌 하
+            cnt += (n*n)//4 * 2
+            divide(n//2, r-(n//2), c)
+        elif r >= n//2 and c >= n//2:                       # 우 하
+            cnt += (n*n)//4 * 3                             # ex) n=8, 각 사각형의 첫값 시작 = 16*i
+            divide(n//2, r-(n//2), c-(n//2))
+
 
 N, r, c = map(int, sys.stdin.readline().split())
 
-ans = divide(2**N, r, c, 0)
-print(ans)
+cnt = 0
+divide(2**N, r, c)
+print(cnt)
