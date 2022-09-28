9c6fa2d92a27a44a98b890966ec17022d247031f
Phangg <109324421+Phangg@users.noreply.github.com>
2022-09-29 01:38:08 +0900

    [Gold V] Title: AC, Time: 460 ms, Memory: 44540 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+
+T = int(sys.stdin.readline())
+for _ in range(T):
+    p = sys.stdin.readline().rstrip()
+    n = int(sys.stdin.readline().rstrip())
+    lst = sys.stdin.readline()[1:-2].split(',')
+
+    q = deque()
+    for i in lst:
+        if i.isdigit():
+            q.append(int(i))
+
+    error = 0
+    r_flag = 0
+    for j in p:
+        if j == 'D':
+            if q:
+                if not r_flag:
+                    q.popleft()
+                else:
+                    q.pop()
+            else:
+                error = 1
+                break
+        else:
+            r_flag = (r_flag+1) % 2
+
+    ans = []
+    if error:
+        print('error')
+    else:
+        if r_flag:
+            while q:
+                ans.append(q.pop())
+        else:
+            while q:
+                ans.append(q.popleft())
+        ans = '[' + ','.join(map(str, ans)) + ']'
+        print(ans)
