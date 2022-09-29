d0ae5c486cbdde4698bf7044025f47509ebdc0ee
Phangg <109324421+Phangg@users.noreply.github.com>
2022-09-29 23:04:17 +0900

    [Gold IV] Title: 최단경로, Time: 1120 ms, Memory: 74256 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,34 @@
+import sys
+import heapq
+from collections import defaultdict
+
+def dij(node):
+    dist = [float('inf')] * (V+1)
+    dist[node] = 0
+    hq = []
+    heapq.heappush(hq, [0, node])
+
+    while hq:
+        now_w, now_v = heapq.heappop(hq)
+        for w, v in graph_dict[now_v]:
+            w += now_w
+            if w < dist[v]:
+                dist[v] = w
+                heapq.heappush(hq, [w, v])
+    return dist
+
+
+V, E = map(int, sys.stdin.readline().split())
+start = int(sys.stdin.readline())
+graph_dict = defaultdict(list)
+for _ in range(E):
+    u, v, w = map(int, sys.stdin.readline().split())
+    graph_dict[u].append([w, v])
+
+x = dij(start)
+
+for t in range(1, len(x)):
+    if x[t] != float('inf'):
+        print(x[t])
+    else:
+        print('INF')
