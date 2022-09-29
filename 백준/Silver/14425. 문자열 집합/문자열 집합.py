e5141a10dd0e2625ad03b04e47cb54a2904acf15
Phangg <109324421+Phangg@users.noreply.github.com>
2022-09-29 21:48:46 +0900

    [Silver III] Title: 문자열 집합, Time: 3752 ms, Memory: 35492 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,9 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+s = [sys.stdin.readline().rstrip() for _ in range(N)]
+cnt = 0
+for _ in range(M):
+    if sys.stdin.readline().rstrip() in s:
+        cnt += 1
+print(cnt)
