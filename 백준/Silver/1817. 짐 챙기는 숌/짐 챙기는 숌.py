6fd126b8d3ff1a9c3f5cbfb801aca11904ec7f47
Phangg <109324421+Phangg@users.noreply.github.com>
2022-10-02 22:13:47 +0900

    [Silver V] Title: 짐 챙기는 숌, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.py
@@ -0,0 +1,19 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+if N == 0:
+    print(0)
+    exit()
+else:
+    books = list(map(int, sys.stdin.readline().split()))[::-1]
+
+box = 1
+weight = 0
+while books:
+    x = books.pop()
+    if x + weight <= M:
+        weight += x
+    else:
+        box += 1
+        weight = x
+print(box)
