2f4127f02a51ab66a39f3b5870c17c9785bf4c54
Phangg <109324421+Phangg@users.noreply.github.com>
2022-10-02 23:24:01 +0900

    [Gold V] Title: 평범한 배낭, Time: 3612 ms, Memory: 33668 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,18 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+
+item = []
+value = [0] * (K+1)
+
+for _ in range(N):
+    W, V = map(int, sys.stdin.readline().split())
+    item.append((W, V))
+
+for i in item:
+    w, v = i
+
+    for t in range(K, w-1, -1):
+        value[t] = max(value[t], value[t-w] + v)
+
+print(value[-1])
