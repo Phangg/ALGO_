a0bd1104b0a81609c2299c8a1ae87f0ca65b1b38
Phangg <109324421+Phangg@users.noreply.github.com>
2022-10-03 23:12:11 +0900

    [Silver III] Title: N과 M (6), Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,18 @@
+import sys
+
+def ncr(s):
+    if len(ans) == M:
+        print(*ans)
+    else:
+        for i in range(s, N):
+            if n_lst[i] not in ans:
+                ans.append(n_lst[i])
+                ncr(i)
+                ans.pop()
+
+N, M = map(int, sys.stdin.readline().split())
+n_lst = list(map(int, sys.stdin.readline().split()))
+n_lst.sort()
+
+ans = []
+ncr(0)
