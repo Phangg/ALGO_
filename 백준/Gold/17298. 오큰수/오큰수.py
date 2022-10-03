35fceeb4c6f4384d42029b0f6979e7e828409fc6
Phangg <109324421+Phangg@users.noreply.github.com>
2022-10-03 15:06:05 +0900

    [Gold IV] Title: 오큰수, Time: 1524 ms, Memory: 189940 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,19 @@
+import sys
+
+N = int(sys.stdin.readline())
+a_lst = list(map(int, sys.stdin.readline().split()))
+
+ans = [-1] * N
+stack = []
+for idx, a in enumerate(a_lst):
+    if not stack:
+        stack.append((idx, a))
+    elif stack[-1][1] >= a:
+        stack.append((idx, a))
+    else:
+        while stack and stack[-1][1] < a:
+            i = stack[-1][0]
+            ans[i] = a
+            stack.pop()
+        stack.append((idx, a))
+print(*ans)
