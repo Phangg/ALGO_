a5a9ffc63f7963bd8bd725b730191cf26ad183db
Phangg <109324421+Phangg@users.noreply.github.com>
2022-10-03 22:53:47 +0900

    [Silver III] Title: N과 M (4), Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,16 @@
+import sys
+
+def f(s):
+    if len(ans) == M:
+        print(*ans)
+    else:
+        for i in range(s, N+1):
+            ans.append(i)
+            f(i)
+            ans.pop()
+
+
+N, M = map(int, sys.stdin.readline().split())
+
+ans = []
+f(1)
