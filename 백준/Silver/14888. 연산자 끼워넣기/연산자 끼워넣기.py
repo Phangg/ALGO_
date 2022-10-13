2be0f22f2fc3eebce62584c1b9ab786e5c56894d
Phangg <109324421+Phangg@users.noreply.github.com>
2022-10-13 22:45:50 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,41 +1,33 @@
 import sys
-from itertools import permutations
 
-def problem():
+# cnt: 계산 횟수 / total: 계산 된 숫자 / p: '+' / m: '-' / x: '*' / y: '/'
+def solve(cnt, total, p, m, x, y):
     global max_v, min_v
-    for x in permutations(op, N-1):                 # 연산자 조합이 나올 수 있는 경우들을 순회 / x : 조합
-        res = A[0]                                  # 결과 값 : 초기 설정 -> 첫 값
-        for k in range(1, N):                       # 첫 값이 이미 들어갔으니, 1부터 순회
-            if x[k-1] == '+':
-                res += A[k]
-            elif x[k-1] == '-':
-                res -= A[k]
-            elif x[k - 1] == '*':
-                res *= A[k]
-            elif x[k-1] == '/':
-                res = int(res / A[k])
-        if res > max_v:
-            max_v = res
-        if res < min_v:
-            min_v = res
+    if cnt == N:
+        max_v = max(max_v, total)
+        min_v = min(min_v, total)
+        return
+
+    if p:
+        solve(cnt+1, total + A[cnt], p-1, m, x, y)
+    if m:
+        solve(cnt+1, total - A[cnt], p, m-1, x, y)
+    if x:
+        solve(cnt+1, total * A[cnt], p, m, x-1, y)
+    if y:
+        solve(cnt+1, int(total / A[cnt]), p, m, x, y-1)
+
 
 
 N = int(sys.stdin.readline())
 A = list(map(int, sys.stdin.readline().split()))
 op_num = list(map(int, input().split()))
-op_list = ['+', '-', '*', '/']
-op = []
-
-# 연산자 개수 별로 op 리스트에 넣기
-for i, o in enumerate(op_num):
-    for j in range(o):
-        op.append(op_list[i])
-# print(op)
+# ['+', '-', '*', '/']
 
 max_v = -1000000001
 min_v = 1000000001
 
-problem()
+solve(1, A[0], op_num[0], op_num[1], op_num[2], op_num[3])
 
 print(max_v)
 print(min_v)
