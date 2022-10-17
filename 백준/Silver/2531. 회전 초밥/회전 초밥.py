8491aeb101389b92ab6fdad662dc1ce7977897f8
Phangg <109324421+Phangg@users.noreply.github.com>
2022-10-18 01:01:41 +0900

    [Silver I] Title: 회전 초밥, Time: 128 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,23 +1,25 @@
 import sys
+from collections import defaultdict
 
 n, d, k, c = map(int, sys.stdin.readline().split())
 lst = list(int(sys.stdin.readline().rstrip()) for _ in range(n))
 
-max_len = 0
-for i in range(n):
-    tmp = set()
-    flag = 0
-    for j in range(k):
-        x = i + j
-        if x >= n:
-            x -= n
-        tmp.add(lst[x])
-        if lst[x] == c:
-            flag = 1
+eat = defaultdict(int)
+cnt = 1
+eat[c] = 1
+for i in range(k):
+    if eat[lst[i]] == 0:
+        cnt += 1
+    eat[lst[i]] += 1
 
-    if max_len <= len(tmp):
-        if flag:
-            max_len = len(tmp)
-        else:
-            max_len = len(tmp)+1
-print(max_len)
+answer = cnt
+for left in range(n):
+    right = (left+k) % n
+    eat[lst[left]] -= 1
+    if eat[lst[left]] == 0:
+        cnt -= 1
+    if eat[lst[right]] == 0:
+        cnt += 1
+    eat[lst[right]] += 1
+    answer = max(answer, cnt)
+print(answer)
