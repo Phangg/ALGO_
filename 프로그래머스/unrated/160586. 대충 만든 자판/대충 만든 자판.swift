3020dcffb0df3a8a91680b2fbb110bd29719b59d
ChangJun Lee (Phang) <109324421+Phangg@users.noreply.github.com>
2023-10-02 00:16:02 +0900

    [unrated] Title: 대충 만든 자판, Time: 9.24 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+func solution(_ keymap: [String], _ targets: [String]) -> [Int] {
+    var keyDictionary = [String: Int]()
+    var keymap = keymap.map { $0.map { String($0) } }
+    var answer = [Int]()
+    
+    for key in keymap {
+        for k in key {
+            if keyDictionary[k] == nil {
+                keyDictionary[k] = key.firstIndex(of: k)! + 1
+            } else {
+                keyDictionary[k] = min(keyDictionary[k]!, key.firstIndex(of: k)! + 1)
+            }
+        }
+    }
+    
+    for target in targets {
+        var res = 0
+        for t in target.map { String($0) } {
+            if keyDictionary[t] == nil {
+                res = -1
+                break
+            } else {
+                res += keyDictionary[t]!
+            }
+        }
+        answer.append(res)
+    }
+    
+    return answer    
+}
